80a98b2343dd89a2be801f3edc459d3fbd35e08f
Irvin Shen <irvin.shen@andromeda360.ai>
2022-10-05 04:40:08 -0500

    Changed placeholder endpoint to a healthcheck

--- src/starpack/main.py
+++ src/starpack/main.py
@@ -3,9 +3,10 @@ import fastapi
 app = fastapi.FastAPI(
     title="Starpack Engine by Andromeda 360",
     description="An extensible engine to package and deploy ML models",
-    version="0.0.1"
+    version="0.0.1",
 )
 
-@app.get("/hello_world")
+
+@app.get("/healthcheck")
 def hello_world():
-    return {"hello": "world"}
+    return {"healthy": True}
--- src/tests/test_endpoint.py
+++ src/tests/test_endpoint.py
@@ -2,11 +2,13 @@ import pytest
 from fastapi.testclient import TestClient
 from src.starpack.main import app
 
+
 @pytest.fixture
 def testclient():
     return TestClient(app)
 
+
 def test_hello_world(testclient):
-    response = testclient.get("/hello_world")
+    response = testclient.get("/healthcheck")
     assert response.status_code == 200
-    assert response.json() == {"hello": "world"}
+    assert response.json() == {"healthy": True}
