8fb42207382f3501c6b025c03c9129a0e5c5be93
Irvin Shen <irvin.shen@andromeda360.ai>
2022-11-24 02:20:42 -0600

    Added additional testcases

--- src/tests/test_endpoint.py
+++ src/tests/test_endpoint.py
@@ -17,3 +17,60 @@ def test_healthcheck(testclient: TestClient) -> None:
 def test_refresh_plugins(testclient: TestClient) -> None:
     response = testclient.patch("/plugins")
     assert response.status_code == 202
+
+
+def test_example_plugin(testclient: TestClient) -> None:
+    response = testclient.post("/plugins/test/example_plugin", json={})
+
+    print(response.content)
+    assert response.status_code == 200
+
+
+fake_input = {
+    "package": {
+        "metadata": {
+            "name": "test",
+            "version": "test"
+        },
+        "artifacts": {
+                "root_location": "test",
+                "inference": {
+                    "function_name": "test",
+                    "script_name": "test"
+                }
+            },
+        "steps": [
+            {"name": "example_plugin"}
+        ]
+    },
+    "deployment": {
+            "metadata": {
+                "name": "test",
+                "version": "test"
+            },
+            "artifacts": {
+                "root_location": "test",
+                "inference": {
+                    "function_name": "test",
+                    "script_name": "test"
+                }
+            },
+            "steps": [
+                {"name": "example_plugin"}
+            ]
+        }
+}
+
+
+def test_deploy_endpoint(testclient: TestClient) -> None:
+    response = testclient.post("/deploy", json=fake_input)
+
+    print(response.content)
+    assert response.status_code == 200
+
+
+def test_package_endpoint(testclient: TestClient) -> None:
+    response = testclient.post("/package", json=fake_input)
+
+    print(response.content)
+    assert response.status_code == 200
