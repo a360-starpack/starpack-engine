286ba561599c87cd75cdc324078c796199269307
Irvin Shen <111306181+irvin-shen-a360@users.noreply.github.com>
2022-12-06 11:25:12 -0600

    Deployment endpoint (#6)
    * Fixed Python imports inside Docker
    
    * Added removal of stale packages
    
    Co-authored-by: Irvin Shen <irvinshen.texas@gmail.com>

---
+++ src/tests/plugins/test_local_docker_deploy.py
@@ -0,0 +1,23 @@
+import pytest
+from fastapi import HTTPException
+
+from src.engine.plugins.local_docker_deploy.main import (
+    find_free_port,
+    delete_duplicate_containers,
+    docker_format_filter,
+    docker_deploy,
+)
+from src.engine.schemas.payloads import Metadata
+
+
+def test_docker_format_filter():
+    test_dict = {
+        "okay": "yeah",
+        "this": "is_cool",
+        "final": "pair"
+    }
+
+    output = ["okay=yeah", "this=is_cool", "final=pair"]
+
+    assert output == docker_format_filter(test_dict)
+
